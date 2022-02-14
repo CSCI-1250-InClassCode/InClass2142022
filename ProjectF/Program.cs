87bb17e4a1a34bfea557a955ccfe3b67177b024b
Mathew Desjardins <desjardinsm@etsu.edu>
2022-02-14 18:45:15 -0500

    Add files via upload

---
+++ ProjectF/Program.cs
@@ -0,0 +1,48 @@
+﻿//bool tOrF = true;
+//false
+
+//1 == 1 //true
+//2 != 3 //true
+//2 != 2 //false
+//2 > 3 //false
+//2 < 3 //true
+//3 >= 3 //true
+//2 <= 3 //true
+
+
+Console.Write("Is it cold outside? (yes / no) "); 
+
+//Yes, yEs, YES, yES, yeS, YeS, YEs, yes
+//yes 
+
+//y
+
+string answer = Console.ReadLine().ToLower();
+
+if(answer != "yes" && answer != "no")
+{
+    System.Console.WriteLine("Come on, follow the instructions!");
+    Console.Write("Is it cold outside? (yes / no) "); 
+    answer = Console.ReadLine().ToLower();
+}
+
+if(answer == "yes")
+{
+    System.Console.WriteLine("Wear a jacket!");
+    System.Console.WriteLine("...mood change...");
+    System.Console.WriteLine("Pull it together and get in the car!");
+    Console.Write("Is it at least sunny? ");
+    string newChoice = Console.ReadLine().ToUpper();
+    if(newChoice == "YES")
+    {
+        System.Console.WriteLine("Maybe it will warm up! :D");
+    }
+    
+}
+else
+{
+    System.Console.WriteLine("I guess it is warm outside!");
+    System.Console.WriteLine("+10 mood, and run outside with wings!!!");
+}
+
+System.Console.WriteLine("Have a nice day.");
---
+++ ProjectG/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Write("What is your current grade? ");
+double grade = double.Parse(Console.ReadLine());
+char letterGrade = 'F';
+
+//if(grade <= 100 && grade >= 90)
+if(grade >= 89.5)
+{
+    letterGrade = 'A';
+}
+else if(grade >= 79.5)
+{
+    letterGrade = 'B';
+}
+//....
+else if(grade >= 69.5)
+{
+    letterGrade = 'C';
+}
+else if(grade >= 59.5)
+{
+    letterGrade = 'D';
+}
+//after if statement
+
+System.Console.WriteLine(letterGrade);
+
+
+System.Console.Write("What is your name? ");
+string name = Console.ReadLine();
+
+switch(name)
+{
+    case "Matt":
+    case "matt":
+        System.Console.WriteLine($"Hello {name}!");
+        break;
+    case "Jone":
+        System.Console.WriteLine($"Hello {name}");
+        break;
+    default:
+        System.Console.WriteLine("Hello whomever you are!");
+        break;
+}
+
+if(name == "Matt" || name == "matt")
+{
+     System.Console.WriteLine($"Hello {name}!");
+}
+else if(name == "Jone")
+{
+     System.Console.WriteLine($"Hello {name}!");
+}
+else
+{
+    System.Console.WriteLine("Hello whomever you are!");
+}
